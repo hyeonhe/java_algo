513c37ab52893ce6367bfb8884606d2d82768b01
Hyunhee Kim <63030323+hyeonhe@users.noreply.github.com>
2025-11-20 11:37:41 +0900

    [Bronze III] Title: 최소, 최대, Time: 440 ms, Memory: 106168 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,23 +1,23 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+
         int n = Integer.parseInt(br.readLine());
+        String[] arr = br.readLine().split(" ");
 
-        st = new StringTokenizer(br.readLine());
-        int min = Integer.parseInt(st.nextToken());
-        int max = min;
+        int min = 1000000;
+        int max = -1000000;
 
-        while(st.hasMoreTokens()) {
-            int num = Integer.parseInt(st.nextToken());
+        for (int i=0; i<n; i++) {
+            int num = Integer.parseInt(arr[i]);
             min = Math.min(min, num);
             max = Math.max(max, num);
         }
+
         System.out.printf("%d %d", min, max);
     }
 }
