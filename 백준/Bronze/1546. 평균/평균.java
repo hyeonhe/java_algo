6a592e87b978ab24b9b0b1bd5a3614bc2445b221
Hyunhee Kim <63030323+hyeonhe@users.noreply.github.com>
2025-11-26 14:17:02 +0900

    [Bronze I] Title: 평균, Time: 112 ms, Memory: 16488 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        double[] arr = new double[n];
+        
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Double.parseDouble(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        double sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum += arr[i] / arr[n - 1] * 100;
+        }
+
+        System.out.println(sum / n);
+    }
+}
