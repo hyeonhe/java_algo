c721b8a214f5389e2cadfb44ccbe620323cca793
Hyunhee Kim <63030323+hyeonhe@users.noreply.github.com>
2025-11-27 11:43:26 +0900

    [Bronze II] Title: 벌집, Time: 116 ms, Memory: 14340 KB -BaekjoonHub

---
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int cnt = 1;
+        int i = 1;
+        int num = 1;
+
+        while (num < n) {
+            num += i++ * 6;
+            cnt++;
+        }
+
+        System.out.println(cnt);
+    }
+
+}
