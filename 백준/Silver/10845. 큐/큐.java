545cf4222cfe7de32368670077ba592fac3c97c7
Hyunhee Kim <63030323+hyeonhe@users.noreply.github.com>
2025-12-06 20:04:03 +0900

    [Silver IV] Title: 큐, Time: 160 ms, Memory: 19028 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.java
+++ 백준/Silver/10845. 큐/큐.java
@@ -1,15 +1,16 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
-        LinkedList<Integer> q = new LinkedList<>();
-
+        Deque<Integer> d = new LinkedList<>();
         int n = Integer.parseInt(br.readLine());
 
         for (int i = 0; i < n; i++) {
@@ -19,25 +20,24 @@ public class Main {
             switch (comm) {
                 case "push":
                     int num = Integer.parseInt(st.nextToken());
-                    q.add(num);
+                    d.addLast(num);
                     break;
                 case "pop":
-                    sb.append(q.isEmpty() ? -1 : q.poll()).append("\n");
+                    sb.append(d.isEmpty() ? -1 : d.poll()).append("\n");
                     break;
                 case "size":
-                    sb.append(q.size()).append("\n");
+                    sb.append(d.size()).append("\n");
                     break;
                 case "empty":
-                    sb.append(q.isEmpty() ? 1 : 0).append("\n");
+                    sb.append(d.isEmpty() ? 1 : 0).append("\n");
                     break;
                 case "front":
-                    sb.append(q.isEmpty() ? -1 : q.peek()).append("\n");
+                    sb.append(d.isEmpty() ? -1 : d.peek()).append("\n");
                     break;
                 case "back":
-                    sb.append(q.isEmpty() ? -1 : q.getLast()).append("\n");
+                    sb.append(d.isEmpty() ? -1 : d.getLast()).append("\n");
                     break;
             }
-
         }
 
         System.out.println(sb);
