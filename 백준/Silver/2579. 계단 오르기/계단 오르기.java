2bded897fc25d9e51823011dfe1d073cfe20df5f
Hyunhee Kim <63030323+hyeonhe@users.noreply.github.com>
2025-12-07 21:45:57 +0900

    [Silver III] Title: 계단 오르기, Time: 100 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -9,15 +9,17 @@ public class Main {
         int n = Integer.parseInt(br.readLine());
         int[] stairs = new int[n+1];
         int[] dp = new int[n+1];
+        
+        stairs[1] = Integer.parseInt(br.readLine());
+        dp[1] = stairs[1];
 
-        for (int i = 1; i <= n; i++) {
-            stairs[i] = Integer.parseInt(br.readLine());
+        if (n >= 2) {
+            stairs[2] = Integer.parseInt(br.readLine());
+            dp[2] = stairs[1] + stairs[2];
         }
 
-        dp[1] = stairs[1];
-        if (n >= 2) dp[2] = stairs[1] + stairs[2];
-
         for (int i = 3; i <= n; i++) {
+            stairs[i] = Integer.parseInt(br.readLine());
             dp[i] = stairs[i] + Math.max(dp[i-2], dp[i-3] + stairs[i-1]);
         }
 
