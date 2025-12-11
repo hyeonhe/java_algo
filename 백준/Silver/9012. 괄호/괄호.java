32ed4dbcd3f577bb97f47e3d9d249d7f01e0f25c
Hyunhee Kim <63030323+hyeonhe@users.noreply.github.com>
2025-12-11 21:32:08 +0900

    [Silver IV] Title: 괄호, Time: 104 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -6,30 +6,32 @@ import java.util.Stack;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Stack<String> stack = new Stack<>();
         StringBuilder sb = new StringBuilder();
-
+        Stack<Character> stack = new Stack<>();
         int t = Integer.parseInt(br.readLine());
+        
         for (int i = 0; i < t; i++) {
-            String str = br.readLine();
+            String str = br.readLine();        
+            boolean flag = true;
+
             for (int j = 0; j < str.length(); j++) {
-                String s = String.valueOf(str.charAt(j));
-                if (s.equals("(")) {
+                char s = str.charAt(j);
+
+                if (s == '(') {
                     stack.add(s);
                 }
-                else if (s.equals(")")) {
+                else if (s == ')') {
                     if (stack.isEmpty()) {
-                        stack.add(s);
+                        flag = false;
                         break;
-                    } else if (stack.pop().equals("(")){
-                        continue;
                     }
+                    stack.pop();
                 }
 
             }
-            if (stack.isEmpty()) sb.append("YES\n");
+            
+            if (flag && stack.isEmpty()) sb.append("YES\n");
             else sb.append("NO\n");
-
             stack.clear();
         }
 
