e72d83df3d27e8aa0edfbbd14d1621b27396ab6b
Hyunhee Kim <63030323+hyeonhe@users.noreply.github.com>
2025-12-15 12:08:45 +0900

    [Bronze II] Title: 복호화, Time: 104 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Bronze/9046. 복호화/복호화.java
+++ 백준/Bronze/9046. 복호화/복호화.java
@@ -10,45 +10,27 @@ public class Main {
         int n = Integer.parseInt(br.readLine());
 
         for (int i = 0; i < n; i++) {
-            // 1. 빈도 계산 (소문자 'a'부터 'z'까지 26개)
-            int[] frequencies = new int[26];
+            int[] alpha = new int[26];
+            int maxNum = 0;
+            int idx = 0;
+            boolean flag = false;
+
             String str = br.readLine();
-
             for (int j = 0; j < str.length(); j++) {
-                char currentChar = str.charAt(j);
-                // 공백 또는 소문자가 아닌 다른 문자를 건너뛰는 로직
-                // 만약 대문자도 처리하려면: char lowerChar = Character.toLowerCase(currentChar);
-                if (currentChar >= 'a' && currentChar <= 'z') {
-                    frequencies[currentChar - 'a']++;
+                if (str.charAt(j) == ' ') continue;
+                alpha[str.charAt(j) - 'a']++;
+            }
+
+            for (int j = 0; j < alpha.length; j++) {
+                if (maxNum < alpha[j]) {
+                    maxNum = alpha[j];
+                    idx = j;
+                    flag = false;
                 }
-                // 기존 코드처럼 공백만 건너뛰려면:
-                // if (currentChar == ' ') continue;
-                // frequencies[currentChar - 'a']++;
+                else if (maxNum == alpha[j]) flag = true;
             }
 
-            // 2. 최대 빈도 찾기
-            int maxFrequency = -1;
-            int maxCharIndex = -1;
-            boolean isTie = false;
-
-            for (int j = 0; j < frequencies.length; j++) {
-                if (frequencies[j] > maxFrequency) {
-                    maxFrequency = frequencies[j];
-                    maxCharIndex = j;
-                    isTie = false; // 새로운 최대 빈도를 찾았으니 동점 아님
-                } else if (frequencies[j] == maxFrequency && maxFrequency > 0) {
-                    // 현재 빈도가 최대 빈도와 같고, 빈도가 0보다 크다면 동점 발생
-                    isTie = true;
-                }
-            }
-
-            // 3. 결과 출력
-            if (isTie) {
-                sb.append("?").append("\n");
-            } else {
-                // maxCharIndex는 'a'로부터의 거리이므로 다시 문자로 변환
-                sb.append(Character.toString(maxCharIndex + 'a')).append("\n");
-            }
+            sb.append(flag ? "?" : Character.toString(idx + 'a')).append("\n");
         }
 
         System.out.println(sb);
