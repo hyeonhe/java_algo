35c083b6bbf475f7b653250d13d70e9183202ad3
Hyunhee Kim <63030323+hyeonhe@users.noreply.github.com>
2025-12-15 10:52:24 +0900

    [Bronze IV] Title: !밀비 급일, Time: 104 ms, Memory: 14080 KB -BaekjoonHub

--- 백준/Bronze/11365. ！밀비 급일/！밀비 급일.java
+++ 백준/Bronze/11365. ！밀비 급일/！밀비 급일.java
@@ -5,16 +5,12 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
 
         while(true) {
             String str = br.readLine();
             if (str.equals("END")) break;
-
-            String reversedWord = new StringBuilder(str).reverse().toString();
-            sb.append(reversedWord).append("\n");
+        
+            System.out.println(new StringBuilder(str).reverse());
         }
-
-        System.out.println(sb);
     }
 }
