b63a57b524d63cc818b98703976ea98f3d91308f
Hyunhee Kim <63030323+hyeonhe@users.noreply.github.com>
2025-12-20 16:46:00 +0900

    [Silver III] Title: 1로 만들기, Time: 100 ms, Memory: 14312 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -6,18 +6,15 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int x = Integer.parseInt(br.readLine());
-        int[] dp = new int[1000001];
 
-        dp[2] = dp[3] = 1;
-        for(int i = 4; i <= 1000000; i++) {
-            int num = Integer.MAX_VALUE;
-            if (i % 3 == 0) num = dp[i / 3] + 1;
-            if (i % 2 == 0) num = Math.min(num, dp[i / 2] + 1);
-            num = Math.min(num, dp[i - 1] + 1);
+        System.out.println(recur(x, 0));
+    }
 
-            dp[i] = num;
+    static int recur(int n, int cnt) {
+        if (n < 2) {
+            return cnt;
         }
 
-        System.out.println(dp[x]);
+        return Math.min(recur(n / 2, cnt + 1 + n % 2), recur(n / 3, cnt + 1 + n % 3));
     }
 }
