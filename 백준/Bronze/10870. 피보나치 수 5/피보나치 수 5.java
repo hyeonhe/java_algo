ed76375ef6d36b99a07b8801a7cd66d067c818f4
Hyunhee Kim <63030323+hyeonhe@users.noreply.github.com>
2026-01-11 16:08:27 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 104 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static char[][] grid;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] dp = new int[21];
+        dp[1] = 1;
+        for (int i = 2; i <= 20; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        System.out.println(dp[Integer.parseInt(br.readLine())]);
+    }
+}
