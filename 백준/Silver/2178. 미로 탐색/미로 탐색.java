cc73dde88684aae8020ac6cb868969fd0e9afe51
Hyunhee Kim <63030323+hyeonhe@users.noreply.github.com>
2026-01-11 16:51:53 +0900

    [Silver I] Title: 미로 탐색, Time: 132 ms, Memory: 15324 KB -BaekjoonHub

---
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -0,0 +1,67 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[] dx = {1, -1, 0, 0};
+    static int[] dy = {0, 0, 1, -1};
+    static int n, m;
+    static int[][] grid;
+    static boolean[][] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        grid = new int[n][m];
+        visited = new boolean[n][m];
+
+        for (int i = 0; i < n; i++) {
+            String line = br.readLine();
+
+            for (int j = 0; j < m; j++) {
+                grid[i][j] = Integer.parseInt(String.valueOf(line.charAt(j)));
+            }
+        }
+
+        bfs();
+        System.out.println(grid[n-1][m-1]);
+    }
+
+    static void bfs() {
+        Queue<Point> queue = new ArrayDeque<>();
+        queue.add(new Point(0, 0));
+        visited[0][0] = true;
+
+        while (!queue.isEmpty()) {
+            Point p = queue.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int nx = p.x + dx[i];
+                int ny = p.y + dy[i];
+
+                if (nx >= 0 && nx < n && ny >= 0 && ny < m && !visited[nx][ny] && grid[nx][ny] != 0) {
+                    queue.add(new Point(nx, ny));
+                    visited[nx][ny] = true;
+                    grid[nx][ny] = grid[p.x][p.y] + 1;
+                }
+            }
+        }
+    }
+}
+
+class Point {
+    int x;
+    int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
