3e18c82ce129157b600a69dba698a489c6477891
Hyunhee Kim <63030323+hyeonhe@users.noreply.github.com>
2026-02-23 10:30:33 +0900

    [Silver II] Title: 최대 힙, Time: 292 ms, Memory: 26580 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            int x = Integer.parseInt(br.readLine());
+
+            if (x == 0) {
+                if (pq.isEmpty()) {
+                    sb.append(0);
+                }
+                else sb.append(pq.poll());
+                sb.append('\n');
+            }
+            else pq.add(x);
+        }
+
+        System.out.println(sb);
+    }
+}
