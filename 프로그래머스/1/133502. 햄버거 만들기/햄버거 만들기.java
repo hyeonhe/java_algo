8fcf88a100b577d5a7d16b3836a6a05e92391f41
Hyunhee Kim <63030323+hyeonhe@users.noreply.github.com>
2026-02-26 20:56:02 +0900

    Update 햄버거 만들기.java
    Refactor solution method to improve readability and maintainability.

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
@@ -2,23 +2,19 @@ import java.util.ArrayList;
 
 class Solution {
     public int solution(int[] ingredient) {
-        int cnt = 0;
-        
+        int cnt = 0;        
         ArrayList<Integer> stack = new ArrayList<>();
         
-        for (int n: ingredient) {
-            
-            
+        for (int n: ingredient) {     
             stack.add(n);
-
             
             if (stack.size() >= 4) {
                 int size = stack.size();
                 
                 if (stack.get(size - 4) == 1 &&
-                   stack.get(size - 3) == 2 &&
-                   stack.get(size - 2) == 3 &&
-                   stack.get(size - 1) == 1) {
+                    stack.get(size - 3) == 2 &&
+                    stack.get(size - 2) == 3 &&
+                    stack.get(size - 1) == 1) {
                     cnt++;
                     
                     for (int i = 0; i < 4; i++) {
